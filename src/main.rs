3ea688a0d3fff52a8d42e1b1d091d530eeb9a98f
KatoMono Enkeli <k4t0mono@gmail.com>
2019-08-05 13:12:12 -0300

    VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -8,18 +10,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HEWO: &[u8] = b"Hewo World! uwu";
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0x0b8000 as *mut u8;   
-
-    for (i, &byte) in HEWO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
 
     loop {}
 }
